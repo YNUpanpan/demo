d771a205a1c0d4e7dc9b9d5be8f3b56999b379e2
YNUpanpan <632344940@qq.com>
2020-06-13 16:11:11 +0800

    first commit

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+ public static void main(String []args){
+ System.out.println("HelloWord1");
+System.out.println("1");
+System.out.println("HelloWord3");
+System.out.println("2");
+}
+}
